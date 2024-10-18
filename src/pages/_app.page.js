ca012959df3638cc9573a617a7b1dde42e0e23b9
Alexis Feron <alexisferon081@gmail.com>
2024-10-18 21:17:30 +0200

    🚑  Fix import

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -1,5 +1,5 @@
-import { SpeedInsights } from '@components/SpeedInsights';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Navbar } from 'components/Navbar';
 import { ThemeProvider } from 'components/ThemeProvider';
 import { tokens } from 'components/ThemeProvider/theme';
